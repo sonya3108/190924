d7d9948a5089172625b5c7025c972f83a7018b66
V win11 <w@example.com>
2024-09-26 16:21:10 +0200

    activate venv

--- LVL_2_Python_group_2024/homework_2.py
+++ LVL_2_Python_group_2024/homework_2.py
@@ -1,19 +1,19 @@
 # 1
-as_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-result = [x for x in as_list if x < 5]
+list_1 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+result = [x for x in list_1 if x < 5]
 print(result)
 
 # 2
-as_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-ba_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-result = list(set(as_list) & set(ba_list))
+list_2 = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+list_3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+result = list(set(list_2) & set(list_3))
 print(result)
 
 
 # 3
-arr = [6, 6, 2, 1, 5, 8, 13, 21, 34, 55, 89]
-bar = [1, 18, 3, 4, 5, 9, 7, 8, 9, 10, 11, 12, 13]
-result = list(set(arr) & set(bar))
+list_4 = [6, 6, 2, 1, 5, 8, 13, 21, 34, 55, 89]
+list_5 = [1, 18, 3, 4, 5, 9, 7, 8, 9, 10, 11, 12, 13]
+result = list(set(list_4) & set(list_5))
 print(result)
 
 
