080ee69ee2c7793bafa5bfc7bae0a130166dc990
vershyna <vershynasofiya2011@gmail.com>
2024-11-11 17:42:21 +0100

    activate venv
    Signed-off-by: vershyna <vershynasofiya2011@gmail.com>

--- LVL_2_Python_group_2024/homework_2.py
+++ LVL_2_Python_group_2024/homework_2.py
@@ -10,20 +10,14 @@
 # print(result)
 
 
-# 3
-def unique_common_elements(arr, bar):
-    common_elements = set(arr).intersection(set(bar))
-
-    result = sorted(common_elements, key=lambda x: (arr.index(x) if x in arr else bar.index(x)))
-
-    return result
-
-
+#3
 arr = [6, 6, 2, 1, 5, 8, 13, 21, 34, 55, 89]
 bar = [1, 18, 3, 4, 5, 9, 7, 8, 9, 10, 11, 12, 13]
 
-result = unique_common_elements(arr, bar)
-print(result)
+print(list(set(arr) ^ set(bar)))
+
+
+
 
 
 
--- LVL_2_Python_group_2024/homework_7.py
+++ LVL_2_Python_group_2024/homework_7.py
@@ -1,85 +1,41 @@
-import re
-from collections import Counter
+"""#7"""
+def count_vowels_letters(text:str, *, isvowel:bool = True)->int:
+    '''
+    Функція обчислює кількість голосних літер або приголосних
+    :param text: текст для пошуку
+    :param isvowel: логічний параметр який налаштовує що саме шукати,
+     якщо True то рахувати тільки голосні літери,
+     або інакше якщо False то шукати тільки приголосні
+    :return: кількість знайдених літер
+    '''
+    return len([sym for sym in text if (sym in 'auioeуеїаоієияю') == isvowel and sym.isalpha()])
 
-def read_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        return file.read()
+def most_popular(text:str)->list:
+    '''
+    Функція приймає текст для обробки
+    :param text: основний текст для пошуку найпопопулярніших літер
+    :return: одну або три найпопулярніших літери, в залежності від кількості голосних чи приголосних літер
+    '''
 
+    text = text.lower()
 
-# Уровень Elementary
-def elementary_analysis(text):
-    text_lower = text.lower()
-    letters = re.findall(r'[a-zа-я]', text_lower)
-    digits = re.findall(r'\d', text_lower)
-    whitespaces = re.findall(r'\s', text_lower)
-    other = re.findall(r'[^\w\s]', text_lower)
-
-    result = {
-        'letters': len(letters),
-        'digits': len(digits),
-        'whitespaces': len(whitespaces),
-        'other': len(other)
-    }
-    return result
-
-
-# Уровень Intermediate
-def intermediate_analysis(text):
-    text_lower = text.lower()
-
-    vowels_set = set("jdgugrbtфів")
-    consonants_set = set("алпршгапркущпрнкупjfhgukdh")
-
-    letters = re.findall(r'[a-zа-я]', text_lower)
-
-    vowels = [letter for letter in letters if letter in vowels_set]
-    consonants = [letter for letter in letters if letter in consonants_set]
-
-    result = {
-        'vowels': len(vowels),
-        'consonants': len(consonants)
-    }
-    return result
+    vow = count_vowels_letters(text)
+    cons = count_vowels_letters(text, isvowel=False)
+    if vow > cons:
+        dict_vowels_letters = {letter : text.count(letter) for letter in text if
+                               letter in 'auioeуеїаоієияю' and letter.isalpha()  }
+        return sorted(dict_vowels_letters, key=dict_vowels_letters.get)[-3:]
+    elif vow < cons:
+        dict_vowels_letters = {letter: text.count(letter) for letter in text if
+                               not letter in 'auioeуеїаоієияю' and letter.isalpha()}
+        return sorted(dict_vowels_letters, key=dict_vowels_letters.get)[-1]
 
 
-# Уровень Advanced
-def advanced_analysis(text):
-    text_lower = text.lower()
+with open('task_7.txt', 'r', encoding='utf-8') as fl:
+    str_1 = fl.read()
 
-    vowels_set = set("рлврппęąćhthhfg")
-    consonants_set = set("gdhuifhrgkdhduенгаупагспус")
+# print(most_popular(str_1))
 
-    letters = re.findall(r'[a-zа-я]', text_lower)
+with open('output.txt', 'w', encoding='utf-8') as fl:
+    fl.write(f'most popular letter(s) is: {most_popular(str_1)}')
 
-    vowels = [letter for letter in letters if letter in vowels_set]
-    consonants = [letter for letter in letters if letter in consonants_set]
-
-    if len(consonants) > len(vowels):
-        most_common_consonant = Counter(consonants).most_common(1)[0][0]
-        result = f"The most frequent consonant: {most_common_consonant}"
-    else:
-
-        most_common_vowels = [item[0] for item in Counter(vowels).most_common(3)]
-        result = f"The three most frequent vowels: {', '.join(most_common_vowels)}"
-
-
-    with open('output.txt', 'w', encoding='utf-8') as output_file:
-        output_file.write(result)
-
-    return result
-
-
-
-text = read_file("C:/Users/Admin/Desktop/190924/LVL_2_Python_group_2024/input_8.txt")
-
-
-elementary_result = elementary_analysis(text)
-print("Elementary result:", elementary_result)
-
-
-intermediate_result = intermediate_analysis(text)
-print("Intermediate result:", intermediate_result)
-
-
-advanced_result = advanced_analysis(text)
-print("Advanced result:", advanced_result)
