fd9fdaf82ce4f5456b49738b1bca8cf622f5617f
vershyna <vershynasofiya2011@gmail.com>
2024-11-12 16:24:20 +0100

    activate venv
    Signed-off-by: vershyna <vershynasofiya2011@gmail.com>

--- LVL_2_Python_group_2024/homework_4.py
+++ LVL_2_Python_group_2024/homework_4.py
@@ -1,8 +1,12 @@
 def move_zeros_to_end(lst):
+    '''
+    Функція переміщує всі нульові елементи (0 або 0.0) в кінець списку, зберігаючи порядок інших елементів.
+    :param lst: список з елементами різних типів
+    :return: новий список, де всі числові нулі знаходяться в кінці
+    '''
     non_zero_elements = [x for x in lst if not (x == 0 and type(x) in [int, float])]
     zero_elements = [x for x in lst if x == 0 and type(x) in [int, float]]
     return non_zero_elements + zero_elements
-
 
 
 print(move_zeros_to_end([False, 1, 0, 1, 2, 0, 1, 3, "a"]))
@@ -10,10 +14,9 @@ print(move_zeros_to_end([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]))
 print(move_zeros_to_end([9, 0.0, 0, 9, 1, 2, 0, 1, 0, 1, 0.0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
 
 
-
 print(move_zeros_to_end(["a", 0, 0, "b", "c", "d", 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
 print(move_zeros_to_end(["a", 0, 0, "b", None, "c", "d", 0, 1, False, 0, 1, 0, 3, [], 0, 1, 9, 0, 0, {}, 0, 0, 9]))
-print(move_zeros_to_end([ 1, None, 2, False, 1, 0]))
+print(move_zeros_to_end([1, None, 2, False, 1, 0]))
 print(move_zeros_to_end(["a", "b"]))
 print(move_zeros_to_end([2, 8]))
 print(move_zeros_to_end([10]))
